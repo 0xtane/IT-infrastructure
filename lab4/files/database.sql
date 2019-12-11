8fe0a215582f0dc382e250cd1b159faf8425adab
saudzi <saudzi@ttu.ee>
2019-12-11 13:18:54 +0200

    removing passwords from repo

--- lab4/files/database.sql
+++ lab4/files/database.sql
@@ -1,5 +1,5 @@
 create database if not exists {{mysql_db}};
 use {{mysql_db}};
-create user if not exists {{mysql_user}}@'%' identified by '{{mysql_pass}}';
+create user if not exists {{mysql_user}}@'%';
 grant all privileges on *.* to {{mysql_user}}@'%';
 flush privileges;
