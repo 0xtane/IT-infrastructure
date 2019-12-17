c54955d56bc37c7eeef36017f415490acf06fadf
saudzi <saudzi@ttu.ee>
2019-12-17 23:33:05 +0200

    removed dir

--- RDM-0.1/files/config-localhost.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("DB_NAME","{{mysql_db}}");
-define("DB_USER","{{mysql_user}}");
-define("DB_PASSWORD","{{mysql_pass}}");
-define("DB_HOST","{{mysql_host}}");
-define("DB_PORT","{{mysql_port}}");
-
-?>
