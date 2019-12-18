8bc59ebff7485f79fb46da7b32d1fab8e2f2daf2
saudzi <saudzi@ttu.ee>
2019-12-18 19:03:52 +0200

    its here

--- exam project/files/database.sql
+++ exam project/files/database.sql
@@ -1,5 +1,5 @@
 create database if not exists {{mysql_db}};
 use {{mysql_db}};
-create user if not exists {{mysql_user}}@'%';
+create user if not exists {{mysql_user}}@'%' identified by '{{mysql_pass}}';
 grant all privileges on *.* to {{mysql_user}}@'%';
 flush privileges;
